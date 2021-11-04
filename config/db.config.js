9e5ff79ab4eb035c9ea89e025605cd97b7098e4f
sribalaji95 <prabhakar.sr@northeastern.edu>
2021-11-04 05:58:12 -0400

    test

---
+++ config/db.config.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql2');
+const bluebird = require('bluebird');
+require('dotenv').config()
+var {db_host, db_user , db_password,  port, default_database} = require('../config.json')
+
+//const [host, port1] = db_host.split(':');
+
+const dbConf = {
+    host     : db_host.split(":")[0],
+    user     : db_user,
+    password : db_password,
+    database : default_database,
+    port : port,
+    Promise: bluebird
+};
+
+class Database {
+
+    static async getDBConnection() {
+        try {
+            if (!this.db) {
+                // to test if credentials are correct
+                await mysql.createConnection(dbConf);
+                
+                const pool = mysql.createPool(dbConf);
+                pool.query("create TABLE IF NOT EXISTS users (first_name varchar(255), last_name varchar(255), id varchar(255),"
+                +"username varchar(255) primary key , password varchar(255), account_created date, account_updated date);");
+                pool.query("create table if not exists image (filename varchar(200), url varchar(250), bucketName varchar(255), "
+                +"username varchar(255),foreign key(username) references users(username), uploaded_date date, img_key varchar(250));")
+                const promisePool = pool.promise();
+                promisePool.query
+                this.db = promisePool;
+            }
+            return this.db;
+        } catch (err) {
+            console.log('Error in database connection');
+            console.log(err.errro || err);
+        }
+
+    }
+}
+
+module.exports = Database;
---
+++ server.js
@@ -0,0 +1,40 @@
+var express    = require("express");
+var login = require('./routes/loginroutes');
+var bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+
+    next();
+});
+var router = express.Router();
+
+// test route
+router.get('/', function(req, res) {
+    res.status(200).send("hellodsdj msdjkdhskdksdh jkdhskjdh")
+    // res.json({ message: 'Test API' });
+});
+
+//route to handle user registration
+router.post('/user',login.register);
+router.get('/user/self',login.getDetails);
+router.put('/user/self',login.update);
+//router.post('/user/self/pic',login.uploadPic);
+router.post('/user/self/pic',login.uploadPic)
+router.get('/user/self/pic',login.viewPic)
+router.delete('/user/self/pic', login.deletePic)
+
+
+app.use('/v1', router);
+app.listen(8000);
+
+module.exports = app
