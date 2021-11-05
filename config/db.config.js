e54e88d1e87b36c5c8c952422a7edf8abf304cff
sribalaji95 <prabhakar.sr@northeastern.edu>
2021-11-05 13:47:08 -0400

    db bug

--- config/db.config.js
+++ config/db.config.js
@@ -21,12 +21,36 @@ class Database {
             if (!this.db) {
                 // to test if credentials are correct
                 await mysql.createConnection(dbConf);
-                
                 const pool = mysql.createPool(dbConf);
-                pool.query("create TABLE IF NOT EXISTS users (first_name varchar(255), last_name varchar(255), id varchar(255),"
-                +"username varchar(255) primary key , password varchar(255), account_created date, account_updated date);");
-                pool.query("create table if not exists image (filename varchar(200), url varchar(250), bucketName varchar(255), "
-                +"username varchar(255),foreign key(username) references users(username), uploaded_date date, img_key varchar(250));")
+                var user_creation ="create TABLE IF NOT EXISTS users (first_name varchar(255), last_name varchar(255), id varchar(255),"
+                +"username varchar(255) primary key , password varchar(255), account_created date, account_updated date);";
+
+                var image_creation = "create table if not exists image (filename varchar(200), url varchar(250), bucketName varchar(255), "
+                +"username varchar(255),foreign key(username) references users(username), uploaded_date date, img_key varchar(250));";
+
+                pool.query("show tables", function (err, result) {
+                if (err) {
+                    console.log("error in showing tables");
+                    throw err;
+                } else {
+                    console.log("tables :", result);
+                    pool.query(user_creation, function (err, result) {
+                    if (err) {
+                        console.log("error in creating user table");
+                        throw err;
+                    } else {
+                        console.log("User Table created");
+                        pool.query(image_creation, function (err, result) {
+                        if (err) {
+                            console.log("error in creating user table");
+                            throw err;
+                        }
+                        console.log("Image Table created");
+                        });
+                    }
+                    });
+                }
+                });
                 const promisePool = pool.promise();
                 promisePool.query
                 this.db = promisePool;
