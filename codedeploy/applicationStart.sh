f5470881eb09a07c26a8d09d9fe492af207ee4e6
sribalaji95 <prabhakar.sr@northeastern.edu>
2021-11-07 15:53:28 -0500

    final234

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cp home/ubuntu/server/config.json home/ubuntu/webapp/
+sudo cp /home/ubuntu/server/config.json /home/ubuntu/webapp/
 cd /home/ubuntu/webapp
 sudo nohup node server.js >> debug.log 2>&1 &
