3d1119f26bcd1c348463cdeb4e4a7bcb571ea546
sribalaji95 <prabhakar.sr@northeastern.edu>
2021-11-07 13:48:16 -0500

    final

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var router = express.Router();
 
 // test route
 router.get('/', function(req, res) {
-    res.status(200).send("sasassdsdds")
+    res.status(200).send("final test")
     // res.json({ message: 'Test API' });
 });
 
