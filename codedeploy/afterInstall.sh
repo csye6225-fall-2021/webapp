6ac7896a00298d887a93a8747f68a407f3bd6f5f
SRIBALAJI PRABHAKAR <prabhakar.sr@northeastern.edu>
2021-11-16 14:46:15 -0500

    cd

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -7,10 +7,4 @@ pwd
 ls -lrt
 cd ..
 sudo cp /home/ubuntu/webapp/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/etc/
-x
-sleep 3
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c /opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json \
-    -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2  -c /opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json -s
